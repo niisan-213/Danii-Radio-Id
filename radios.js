b86986af39005db09dfc2dcf725194dbe2a1d81e
niisan-213 <yumehanii@au.com>
2026-08-30 10:14:11 +0900

    Add files via upload

---
+++ radios.js
@@ -0,0 +1,329 @@
+const radios = [
+
+{
+    name: "MESMERIZER / メズマライザー (Clown Remix)",
+    id: "71934965392436",
+    type: "music",
+    mp3: "sounds/71934965392436.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/71934965392436/MESMERIZER-Clown-Remix",
+    officialUrl:
+        "https://youtu.be/ZMtrVf3ncmA",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "nMisaki - Dubidubidu (Uptempo Remix)",
+    id: "16190783444",
+    type: "music",
+    mp3: "sounds/16190783444.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/16190783444/nMisaki-Dubidubidu-Uptempo-Remix",
+    officialUrl:
+        "https://youtu.be/ld9rLLLBYmI?si=B5YdQwmz5Jm3RDuA",
+    tags: [
+        "ミーム"
+    ]
+},
+
+{
+    name: "F-L-Y | Hatsune Miku【Vocaloid Reimagined】Day 1",
+    id: "76819270320985",
+    type: "music",
+    mp3: "sounds/76819270320985.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/76819270320985/FLY-Hatsune-MikuVocaloid-ReimaginedDay-1",
+    officialUrl:
+        "https://youtu.be/6t4qe1ZcmKI?si=nvEwxJyD1jPrDOuw",
+    tags: [
+        "ボカロ"
+    ]
+},
+
+{
+    name: "Monitoring Remix (inspired by Deco27)",
+    id: "92292285830973",
+    type: "music",
+    mp3: "sounds/92292285830973.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/92292285830973/Monitoring-Remix-inspired-by-Deco27",
+    officialUrl:
+        "https://youtu.be/8M8sJINVlAQ?si=Gi-mVunCoyIBHms0",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "CUTEMAKMAKFUNK (Slowed)",
+    id: "120871403922972",
+    type: "music",
+    mp3: "sounds/120871403922972.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/120871403922972/CUTEMAKMAKFUNK-Slowed",
+    officialUrl:
+        "https://youtu.be/abwf-BdPFsQ?si=oS-r8xhUNXreV9On",
+    tags: [
+        "日本",
+        "ボカロ",
+        "Phonk"
+    ]
+},
+
+{
+    name: "Parry Gripp - Raining Tacos",
+    id: "142376088",
+    type: "music",
+    mp3: "sounds/142376088.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/142376088/Parry-Gripp-Raining-Tacos",
+    officialUrl:
+        "https://youtu.be/npjF032TDDQ?si=RWPLV-2XdcH5KjBN",
+    tags: []
+},
+
+{
+    name: "RainTemple",
+    id: "110829045185545",
+    type: "music",
+    mp3: "sounds/110829045185545.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/110829045185545/RainTemple",
+    officialUrl: "",
+    tags: [
+        "日本"
+    ]
+},
+
+{
+    name: "Looping In The Backrooms (Remix)",
+    id: "76580060470689",
+    type: "music",
+    mp3: "sounds/76580060470689.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/76580060470689/Looping-In-The-Backrooms-Remix",
+    officialUrl: "",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "melodia de verão (tiktok edit)",
+    id: "118507373399694",
+    type: "music",
+    mp3: "sounds/118507373399694.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/118507373399694/melodia-de-vero-tiktok-edit",
+    officialUrl:
+        "https://youtu.be/eN54PQhfRc8?si=XGSKeZ2asyL4cMAN",
+    tags: [
+        "Phonk"
+    ]
+},
+
+{
+    name: "Monet",
+    id: "88583608079509",
+    type: "music",
+    mp3: "sounds/88583608079509.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/88583608079509/Monet",
+    officialUrl: "",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "kyu-kurarin64",
+    id: "114087038177872",
+    type: "music",
+    mp3: "sounds/114087038177872.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/114087038177872/kyukurarin64",
+    officialUrl:
+        "https://www.youtube.com/watch?v=uZfOFOVIGhE&list=RDuZfOFOVIGhE&start_radio=1",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "ハナタバ",
+    id: "132966669971723",
+    type: "music",
+    mp3: "sounds/132966669971723.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/132966669971723/%E3%83%8F%E3%83%8A%E3%82%BF%E3%83%90",
+    officialUrl: "",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "Pegboard Nerds & Tokyo Machine - Moshi",
+    id: "7024340270",
+    type: "music",
+    mp3: "sounds/7024340270.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/7024340270/Pegboard-Nerds-Tokyo-Machine-Moshi",
+    officialUrl:
+        "https://youtu.be/NgPWqeirmrM?si=VRJ5ePvXyL0UwIcu",
+    tags: []
+},
+
+{
+    name: "Solar Drive",
+    id: "140498777165558",
+    type: "music",
+    mp3: "sounds/140498777165558.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/140498777165558/Solar-Drive",
+    officialUrl: "",
+    tags: [
+        "日本"
+    ]
+},
+
+{
+    name: "すろーりーないと",
+    id: "72999239742034",
+    type: "music",
+    mp3: "sounds/72999239742034.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/72999239742034/すろーりーないと",
+    officialUrl: "",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "トリックハート",
+    id: "123268426492417",
+    type: "music",
+    mp3: "sounds/123268426492417.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/123268426492417/トリックハート",
+    officialUrl: "",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "くうになる",
+    id: "138077217004132",
+    type: "music",
+    mp3: "sounds/138077217004132.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/138077217004132/くうになる",
+    officialUrl: "",
+    tags: [
+        "日本",
+        "ボカロ"
+    ]
+},
+
+{
+    name: "やっぱり俺の青春ラブコメに何の問題もね",
+    id: "93800438751265",
+    type: "music",
+    mp3: "sounds/93800438751265.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/93800438751265/やっぱり俺の青春ラブコメに何の問題もね",
+    officialUrl: "",
+    tags: [
+    ]
+},
+
+{
+    name: "huh car cat",
+    id: "132762282841552",
+    type: "effect",
+    mp3: "sounds/132762282841552.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/132762282841552/huh-car-cat",
+    officialUrl: "",
+    tags: [
+        "ミーム"
+    ]
+},
+
+{
+    name: "iphone alarm [loud]",
+    id: "4203251375",
+    type: "effect",
+    mp3: "sounds/4203251375.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/4203251375/iphone-alarm-loud",
+    officialUrl: "",
+    tags: [
+    ]
+},
+
+{
+    name: "FindExit",
+    id: "71186345039796",
+    type: "music",
+    mp3: "sounds/71186345039796.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/71186345039796/FindExit",
+    officialUrl: "",
+    tags: [
+    ]
+},
+
+{
+    name: "Good Android",
+    id: "123572020022002",
+    type: "music",
+    mp3: "sounds/123572020022002.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/123572020022002/Good-Android",
+    officialUrl: "",
+    tags: [
+        "日本",
+        "東方"
+    ]
+},
+
+{
+    name: "Science Mysteries",
+    id: "1840776993",
+    type: "music",
+    mp3: "sounds/1840776993.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/1840776993/Science-Mysteries",
+    officialUrl: "",
+    tags: [
+        "ゲーム"
+    ]
+},
+
+{
+    name: "READY OR NOT (SCH00LKIDD MIX)",
+    id: "119731837417100",
+    type: "music",
+    mp3: "sounds/119731837417100.mp3",
+    soundSourceUrl:
+        "https://create.roblox.com/store/asset/119731837417100/READY-OR-NOT-SCH00LKIDD-MIX",
+    officialUrl: "",
+    tags: [
+        "ゲーム"
+    ]
+}
+
+];
